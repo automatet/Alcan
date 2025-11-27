4b80e2ac738f53488c92dc20489016b8ec4b8cf1
Sharma Himanshu <sharma.himanshu@newvisionsoftpune.com>
2025-11-27 15:10:24 +0530

    our first commit

---
+++ pages/loginPage.js
@@ -0,0 +1,28 @@
+import testdata from '../testdata.json'
+
+class LoginPage{
+
+    constructor(page){
+
+        this.page = page;
+        this.useremail = page.locator("//input[@id='email']")
+        this.userpasword = page.locator("//input[@id='password']")
+        this.captchabox = page.locator("//div[@class='captcha-box']")
+        this.captcha = page.locator("//input[@id='captcha']")
+        this.loginbtn = page.locator("//button[@type='submit']")
+    }
+
+    async login(){
+
+        await this.useremail.fill(testdata.email)
+        await this.userpasword.fill(testdata.password)
+        const captchaText =await this.captchabox.innerText();
+        await this.captcha.fill(captchaText.trim())
+        await this.loginbtn.click()
+
+      
+
+    }
+}
+
+export default LoginPage;
