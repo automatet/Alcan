e0bd7dacdbd460eb09bb937657f401ab95d90af0
Prashant940 <prashant.chouksey@newvision-software.com>
2025-12-02 16:06:33 +0530

    Add Playwright 4 test cases

--- pages/notificationPage.js
+++ pages/notificationPage.js
@@ -1,3 +1,4 @@
+import { expect } from '@playwright/test';
 import testdata from '../testdata.json'
 import testdataNotification from '../tests/notificationData.json';
 import notificationData from '../tests/notificationData.json';
@@ -10,6 +11,8 @@ class NotificationPage{
         this.Notification_Management_Tab = page.locator("//span[contains(text(),'Notification Management')]")
         this.Create_New = page.locator("//h4[contains(text(),'Create New')]")
         this.New_Campaign_Name = page.locator("//input[@id='campaignName']")
+        this.New_email_Text = page.locator("//*[@id='emailText']")
+        this.New_email_Subject = page.locator("//*[@id='emailSubject']")        
         this.New_Campaign_Duration_Start_Date = page.locator("//input[@id='startDate']")
         this.New_Campaign_Duration_End_Date = page.locator("//input[@id='endDate']")
         this.New_In_App_Notification = page.locator("//input[@value='inApp']")
@@ -21,35 +24,128 @@ class NotificationPage{
         this.New_Mobile_Preview = page.locator("//span[contains(text(),'Mobile Preview')]")
         this.New_Save_as_Draft = page.locator("//span[contains(text(),'Save as Draft')]")
         this.New_Publish = page.locator("//span[contains(text(),'Publish')]")
-       this.New_Mobile_Preview_Campaign_Name = page.locator("//div[contains(text(),'${notificationData.campaignName}')]")
-        this.New_Mobile_Preview_Description = page.locator("//div[contains(text(),'${notificationData.description}')]")
+       this.New_Mobile_Preview_Campaign_Name = page.locator("//*[@id='root']/div/div/main/div[4]/div[1]/div/div[3]/div[1]/div[1]")
+       this.New_Mobile_Preview_Description = page.locator("//*[@id='root']/div/div/main/div[4]/div[1]/div/div[3]/div[2]/div")
         
+        this.Drafts = page.locator("//h4[contains(text(),'Drafts')]")
+        this.Drafts_text= page.locator("//*[@id='root']/div/div/main/div/div[1]/div[1]/div/div[2]/div[1]/div[1]/h5")
+        this.Drafts_text_edit= page.locator(`//h5[contains(text(),"${notificationData.campaignName}")]/ancestor::div[2]/div[2]/button[1]`)
+        this.Drafts_Campaign_Name = page.locator("//input[@id='campaignName']")
+        this.Drafts_Campaign_Duration_Start_Date = page.locator("//input[@id='startDate']")
+        this.Drafts_Campaign_Duration_End_Date = page.locator("//input[@id='endDate']")
+        this.Drafts_Notification_Type = page.locator("//span[text()='Notification Type']")
+        this.Drafts_In_App_Notification = page.locator("//input[@value='inApp']")
+        this.Drafts_Push_Notification = page.locator("//input[@value='push']")
+        this.Drafts_Email_Notification = page.locator("//input[@value='email']")
+        this.Drafts_Title = page.locator("//input[@id='title']")
+        this.Drafts_Subtitle = page.locator("//input[@id='subtitle']")
+        this.Drafts_Subject_Line = page.locator("")
+        this.Drafts_Email_Text = page.locator("//*[@id='emailText']")
+        this.Drafts_Description = page.locator("//p[@data-placeholder='Enter notification description']")
+        this.Draft_Notification = page.locator("")
+        this.Draft_Mobile_Preview = page.locator("//span[contains(text(),'Mobile Preview')]")
+        this.Draft_Save_as_Draft = page.locator("//span[contains(text(),'Save as Draft')]")
+        this.Draft_Publish = page.locator("//span[contains(text(),'Publish')]")
+       // this.Draft_Delete = page.locator("//h5[contains(text(),${notificationData.campaignName})]/ancestor::div[2]/div[2]/button[2]")
+        this.Draft_Delete = page.locator(`//h5[contains(text(),"${notificationData.campaignName}")]/ancestor::div[2]/div[2]/button[2]`)
+        this.Draft_View = page.locator(`//h5[contains(text(),"${notificationData.campaignName}")]/ancestor::div[2]/div[2]/button[3]`)
+        this.Draft_Delete_Conform_Yes = page.locator("//span[contains(text(),'Yes')]")
+        this.Draft_Noti_Campaign_Name = page.locator("//div[@class='ant-card-body']/div/div/h5")
+        
+
 
     }
 
-    async CreateMobileNotification(){
+    async CreateMobileNotification_Mobile_Preview(){
 
         await this.Notification_Management_Tab.click()
         await this.Create_New.click()
         await this.New_Campaign_Name.fill(notificationData.campaignName)
+   
+        await this.New_Campaign_Duration_Start_Date.fill(notificationData.StartDateTime);
+        await this.New_Campaign_Duration_End_Date.fill(notificationData.EndDateTime);
+        await this.New_In_App_Notification.check()
+        await this.New_Push_Notification.check()
+        await this.New_Email_Notification.check()
+        await this.New_email_Text.fill(notificationData.emailText)
+        await this.New_email_Subject.fill(notificationData.emailSubject)
+        await this.New_Title.fill(notificationData.title)
+        await this.New_Subtitle.fill(notificationData.subTitle)
+        await this.New_Description.fill(notificationData.description)
+        await this.New_Mobile_Preview.click()
         
+        const previewCampaignName = await this.New_Mobile_Preview_Campaign_Name.innerText();
+        console.log("Preview Campaign Name: "+previewCampaignName);
+        await expect(previewCampaignName).toBe(notificationData.campaignName);
+       
+        const previewDescription = await this.New_Mobile_Preview_Description.innerText();
+        console.log("Preview Description: "+previewDescription);
+        await expect(previewDescription).toBe(notificationData.description);
+       
+        await expect(this.New_In_App_Notification).toBeChecked();
+        await expect(this.New_Push_Notification).toBeChecked();
+        await expect(this.New_Email_Notification).toBeChecked();
+       
+       
+    }
+
+
+ async CreateMobileNotification_Save_as_Draft(){
+
+        await this.Notification_Management_Tab.click()
+        await this.Create_New.click()
+        await this.New_Campaign_Name.fill(notificationData.campaignName)      
         await this.New_Campaign_Duration_Start_Date.fill(notificationData.StartDateTime);
         await this.New_Campaign_Duration_End_Date.fill(notificationData.EndDateTime);
 
         await this.New_In_App_Notification.check()
         await this.New_Push_Notification.check()
         await this.New_Email_Notification.check()
-      //  await expect(this.New_In_App_Notification).toBeChecked();
-      //  await expect(this.New_Push_Notification).toBeChecked();
-      //  await expect(this.New_Email_Notification).toBeChecked();
+         await this.New_email_Text.fill(notificationData.emailText)
+        await this.New_email_Subject.fill(notificationData.emailSubject)
+        
         await this.New_Title.fill(notificationData.title)
         await this.New_Subtitle.fill(notificationData.subTitle)
-        await this.New_Description.fill(notificationData.description)
-        await this.New_Mobile_Preview.click()
-        await expect(this.New_Mobile_Preview_Campaign_Name).toHaveVisiable();
-        await expect(this.New_Mobile_Preview_Description).toHaveVisiable();
+        await this.New_Description.fill(notificationData.description)       
+        await expect(this.New_In_App_Notification).toBeChecked();
+        await expect(this.New_Push_Notification).toBeChecked();
+        await expect(this.New_Email_Notification).toBeChecked();
+        await this.New_Save_as_Draft.click();
 
+      
     }
+    async CreateMobileNotification_Publish(){
+
+        await this.Notification_Management_Tab.click()
+        await this.Create_New.click()
+        await this.New_Campaign_Name.fill(notificationData.campaignName)      
+        await this.New_Campaign_Duration_Start_Date.fill(notificationData.StartDateTime);
+        await this.New_Campaign_Duration_End_Date.fill(notificationData.EndDateTime);
+
+        await this.New_In_App_Notification.check()
+        await this.New_Push_Notification.check()
+        await this.New_Email_Notification.check()
+         await this.New_email_Text.fill(notificationData.emailText)
+        await this.New_email_Subject.fill(notificationData.emailSubject)
+        
+        await this.New_Title.fill(notificationData.title)
+        await this.New_Subtitle.fill(notificationData.subTitle)
+        await this.New_Description.fill(notificationData.description)       
+        await expect(this.New_In_App_Notification).toBeChecked();
+        await expect(this.New_Push_Notification).toBeChecked();
+        await expect(this.New_Email_Notification).toBeChecked();
+        await this.New_Publish.click();
+
+      
+    }
+     async Delete_Draft_Notification(){
+
+       await NotificationManagement.Drafts.click()
+   const draftText = await NotificationManagement.Drafts_text.textContent()
+   expect(draftText).toBe(notificationData.campaignName)   
+      
+    }
+
 }
 
 export default NotificationPage;
--- tests/notification.spec.js
+++ tests/notification.spec.js
@@ -3,25 +3,68 @@ import LoginPage from '../pages/loginPage.js';
 import testdata from '../testdata.json';
 import notificationPage from '../pages/NotificationPage.js';
 import notificationData from '../tests/notificationData.json';
+import { fail } from 'assert';
+const { faker } = require('@faker-js/faker');
+const fs = require('fs');
 
-test('verify Notification Management', async ({page})=>{
-
-    
+test('Create Mobile Notification with Mobile Preview', async ({page})=>{
+ 
   const loginuser = new LoginPage(page)
     await page.goto(testdata.URL)
-
     await loginuser.login()
-
    const NotificationManagement = new notificationPage(page)
-   await NotificationManagement.CreateMobileNotification()
-  
-        
-  
-
-
-
-   
-
-
+   await NotificationManagement.CreateMobileNotification_Mobile_Preview()
+ 
+})
+test('Create Mobile Notification with Save as Draft ', async ({page})=>{
+ 
+  const loginuser = new LoginPage(page)
+    await page.goto(testdata.URL)
+    await loginuser.login()
+   const NotificationManagement = new notificationPage(page)
+   await NotificationManagement.CreateMobileNotification_Save_as_Draft()
+    await NotificationManagement.Delete_Draft_Notification;
 
 })
+test('Create Mobile Notification with publish ', async ({page})=>{
+ 
+  const loginuser = new LoginPage(page)
+    await page.goto(testdata.URL)
+    await loginuser.login()
+   const NotificationManagement = new notificationPage(page)
+   await NotificationManagement.CreateMobileNotification_Publish()
+})
+test('delete draft notification', async ({page})=>{
+ 
+  const loginuser = new LoginPage(page)
+    await page.goto(testdata.URL)
+    await loginuser.login()
+   const NotificationManagement = new notificationPage(page)
+   await NotificationManagement.CreateMobileNotification_Save_as_Draft()
+   await NotificationManagement.Drafts.click()
+   const draftText = await NotificationManagement.Drafts_text.textContent()
+   expect(draftText).toBe(notificationData.campaignName)    
+    await NotificationManagement.Draft_Delete.click()
+    await NotificationManagement.Draft_Delete_Conform_Yes.click()
+    
+
+if (NotificationManagement.Draft_Noti_Campaign_Name !== notificationData.campaignName) {
+    console.log("Validation Passed - Values are NOT equal");
+    
+} else {
+    console.log("Validation Failed - Values are equal");
+    fail();
+}
+
+})
+test('verify draft notification added', async ({page})=>{
+ 
+  const loginuser = new LoginPage(page)
+    await page.goto(testdata.URL)
+    await loginuser.login()
+    const NotificationManagement = new notificationPage(page)
+   await NotificationManagement.CreateMobileNotification_Save_as_Draft()
+
+    await NotificationManagement.Delete_Draft_Notification;
+})
+
