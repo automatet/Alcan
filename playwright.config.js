46b66e2384fe5638a72927fa0e1648cea71f8447
Sharma Himanshu <sharma.himanshu@newvisionsoftpune.com>
2025-12-09 14:38:16 +0530

    Add and integrated Github Actions and Allure report and screenshot

--- playwright.config.js
+++ playwright.config.js
@@ -15,6 +15,7 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
+
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   // @ts-ignore
@@ -34,7 +35,9 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+  screenshot: 'only-on-failure',
+  video: 'retain-on-failure',
+  trace: 'on-first-retry',
   },
 
   /* Configure projects for major browsers */
